2cf4af48a7ee613c250345f01df2463577892182
Ulrich Ngachou <ulrichlandry722@gmail.com>
2026-05-18 09:47:28 +0100

    Modification axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,12 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-<<<<<<< HEAD
-  baseURL: process.env.NEXT_API_BASE_URL || "http://192.168.43.150:3000/",
-=======
   baseURL: process.env.NEXT_API_BASE_URL || "http://192.168.43.150:3000",
->>>>>>> DRAKANE
-  withCredentials: true,
 });
 
 export default axiosInstance;
