5bf8c4aa438a8c36836a9e7533ea2aa9380c7ebb
Ulrich Ngachou <ulrichlandry722@gmail.com>
2026-05-18 09:57:56 +0100

    modify

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
+<<<<<<< Updated upstream
   baseURL: process.env.NEXT_API_BASE_URL || "http://192.168.43.150:3000",
+=======
+  baseURL: process.env.NEXT_API_BASE_URL || "http://192.168.43.150:3000/",
+  withCredentials: true,
+>>>>>>> Stashed changes
 });
 
 export default axiosInstance;
