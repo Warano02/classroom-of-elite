8e8132b7e332933d9a0ade51cedccdcd1d02d8e1
warano02 <carineteoi@gmail.com>
2026-06-03 12:21:19 +0100

    feat: notification page

--- app/user/(root)/notifications/page.tsx
+++ app/user/(root)/notifications/page.tsx
@@ -1,4 +1,6 @@
 import { Metadata } from "next"
+import { Suspense } from "react"
+import NotificationsPage from "./client"
 
 export const metadata: Metadata = {
     title: "Notifications",
@@ -7,7 +9,9 @@ export const metadata: Metadata = {
 
 function Notifications() {
     return (
-        <div>Notifications</div>
+        <Suspense fallback={<div className="flex items-center justify-center h-screen">Loading...</div>}>
+            <NotificationsPage/>
+        </Suspense>
     )
 }
 
